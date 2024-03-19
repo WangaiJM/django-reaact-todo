d4e7b155d66c6f820dde89517f1bf0fc1d88bb5d
WangaiJM <jahkeyjohn@gmail.com>
2024-03-20 00:45:50 +0300

    backend restframework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,11 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
+    # external
+    'rest_framework',
+    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
---
+++ backend/todo/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('todo', views.TodoViewSet)
+urlpatterns = []
+urlpatterns += router.urls
