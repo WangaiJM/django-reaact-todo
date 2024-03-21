9afb28128bbb4abf66c1944b9f9af6090781fd2b
WangaiJM <jahkeyjohn@gmail.com>
2024-03-21 05:03:26 +0300

    integrating backend with frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Table from "./components/Table";
 import TodoForm from "./components/TodoForm";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/todo/");
+      setTodos(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="bg-indigo-100 min-h-screen p-4">
       <nav className="pt-8">
         <h1 className="text-5xl text-center"> ToDo List</h1>
       </nav>
       <TodoForm />
-      <Table />
+      <Table todos={todos} setTodos={setTodos} isLoading={isLoading} />
     </div>
   );
 }
--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,10 +1,16 @@
 import {
   MdCheckBoxOutlineBlank,
   MdEditNote,
+  MdOutlineCheckBox,
   MdOutlineDeleteOutline,
 } from "react-icons/md";
-const Table = () => {
-  return (
+/* eslint-disable react/prop-types */
+const Table = ({ todos, setTodos, isLoading }) => {
+  return isLoading ? (
+    <div className="flex justify-center m-32">
+      <p>Loading</p>
+    </div>
+  ) : (
     <div className="py-32 flex justify-center">
       <table className="w-11/12 max-w-4xl">
         <thead className="border-b-2 border-black">
@@ -27,116 +33,40 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="p-3 text-sm ">
-              <span className="cursor-pointer inline-block">
-                <MdCheckBoxOutlineBlank />
-              </span>
-            </td>
-            <td className="p-3 text-sm ">Lorem ipsum dolor sit.</td>
-            <td className="p-3 text-sm">
-              <span className="p-1.5 text-xs text-center font-medium tracking-wider rounded-md bg-green-300">
-                Done
-              </span>
-            </td>
-            <td className="p-3 text-sm ">22-04-24</td>
-            <td className="p-3 text-sm font-medium grid grid-flow-col items-center mt-5">
-              <span className=" cursor-pointer text-xl">
-                <MdEditNote />
-              </span>
-              <span className=" cursor-pointer text-xl">
-                <MdOutlineDeleteOutline />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-3 text-sm ">
-              <span className="cursor-pointer inline-block">
-                <MdCheckBoxOutlineBlank />
-              </span>
-            </td>
-            <td className="p-3 text-sm ">Lorem ipsum dolor sit.</td>
-            <td className="p-3 text-sm">
-              <span className="p-1.5 text-xs text-center font-medium tracking-wider rounded-md bg-green-300">
-                Done
-              </span>
-            </td>
-            <td className="p-3 text-sm ">22-04-24</td>
-            <td className="p-3 text-sm font-medium grid grid-flow-col items-center mt-5">
-              <span className=" cursor-pointer text-xl">
-                <MdEditNote />
-              </span>
-              <span className=" cursor-pointer text-xl">
-                <MdOutlineDeleteOutline />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-3 text-sm ">
-              <span className="cursor-pointer inline-block">
-                <MdCheckBoxOutlineBlank />
-              </span>
-            </td>
-            <td className="p-3 text-sm ">Lorem ipsum dolor sit.</td>
-            <td className="p-3 text-sm">
-              <span className="p-1.5 text-xs text-center font-medium tracking-wider rounded-md bg-green-300">
-                Done
-              </span>
-            </td>
-            <td className="p-3 text-sm ">22-04-24</td>
-            <td className="p-3 text-sm font-medium grid grid-flow-col items-center mt-5">
-              <span className=" cursor-pointer text-xl">
-                <MdEditNote />
-              </span>
-              <span className=" cursor-pointer text-xl">
-                <MdOutlineDeleteOutline />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-3 text-sm ">
-              <span className="cursor-pointer inline-block">
-                <MdCheckBoxOutlineBlank />
-              </span>
-            </td>
-            <td className="p-3 text-sm ">Lorem ipsum dolor sit.</td>
-            <td className="p-3 text-sm">
-              <span className="p-1.5 text-xs text-center font-medium tracking-wider rounded-md bg-green-300">
-                Done
-              </span>
-            </td>
-            <td className="p-3 text-sm ">22-04-24</td>
-            <td className="p-3 text-sm font-medium grid grid-flow-col items-center mt-5">
-              <span className=" cursor-pointer text-xl">
-                <MdEditNote />
-              </span>
-              <span className=" cursor-pointer text-xl">
-                <MdOutlineDeleteOutline />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-3 text-sm ">
-              <span className="cursor-pointer inline-block">
-                <MdCheckBoxOutlineBlank />
-              </span>
-            </td>
-            <td className="p-3 text-sm ">Lorem ipsum dolor sit.</td>
-            <td className="p-3 text-sm">
-              <span className="p-1.5 text-xs text-center font-medium tracking-wider rounded-md bg-green-300">
-                Done
-              </span>
-            </td>
-            <td className="p-3 text-sm ">22-04-24</td>
-            <td className="p-3 text-sm font-medium grid grid-flow-col items-center mt-5">
-              <span className=" cursor-pointer text-xl">
-                <MdEditNote />
-              </span>
-              <span className=" cursor-pointer text-xl">
-                <MdOutlineDeleteOutline />
-              </span>
-            </td>
-          </tr>
+          {todos.map((todoItem, index) => {
+            return (
+              <tr key={index}>
+                <td className="p-3 text-sm ">
+                  <span className="cursor-pointer inline-block">
+                    {todoItem.completed ? (
+                      <MdOutlineCheckBox />
+                    ) : (
+                      <MdCheckBoxOutlineBlank />
+                    )}
+                  </span>
+                </td>
+                <td className="p-3 text-sm ">{todoItem.body}</td>
+                <td className="p-3 text-sm">
+                  <span
+                    className={`p-1.5 text-xs text-center font-medium tracking-wider rounded-md ${
+                      todoItem.completed ? "bg-green-300" : "bg-red-300"
+                    }`}
+                  >
+                    {todoItem.completed ? "Done" : "In Progress"}
+                  </span>
+                </td>
+                <td className="p-3 text-sm ">{todoItem.created}</td>
+                <td className="p-3 text-sm font-medium grid grid-flow-col items-center mt-5">
+                  <span className=" cursor-pointer text-xl">
+                    <MdEditNote />
+                  </span>
+                  <span className=" cursor-pointer text-xl">
+                    <MdOutlineDeleteOutline />
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
